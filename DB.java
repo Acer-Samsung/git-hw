7593b7d04f13d04a262cbb3213f31abaa5e3ed3f
Acer-Samsung <bahtiyorov1907@gmail.com>
2025-01-22 14:27:09 +0500

    added fields to db

--- DB.java
+++ DB.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DB {
+    private String dbName;
     private List<String> users = new ArrayList<>();
     private List<String> comments = new ArrayList<>();
 
