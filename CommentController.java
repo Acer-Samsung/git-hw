ed7cb7ea3a45023d421eaa61aba0bb4d425d92d6
Acer-Samsung <bahtiyorov1907@gmail.com>
2025-01-22 12:15:35 +0500

    created Main class and completed CommentController

--- CommentController.java
+++ CommentController.java
@@ -4,15 +4,28 @@ class Comment {
     int id = 1;
     int user_id;
     String comment;
+
+    public Comment(int user_id, String comment) {
+        this.user_id = user_id;
+        this.comment = comment;
+    }
+    public Comment(){}
 }
 
 public class CommentController {
-    public void addComment(int id, int user_id, String comment) {
-        id += 1;
+
+    public CommentController(DB db) {this.db =db;}
+
+    private DB db;
+
+    public void addComment(Comment comment) {
+        comment.id = comment.id + 1;
         Scanner sc = new Scanner(System.in);
         System.out.println("enter user id: ");
-        sc.nextInt();
+        comment.user_id = sc.nextInt();
         System.out.println("Enter comment: ");
-        sc.nextLine();
+        Scanner sc2 = new Scanner(System.in);
+        comment.comment = sc2.nextLine();
+        db.getComments().add("id = "+ comment.id + " userid = " + comment.user_id + " comment = " + comment.comment);
     }
 }
--- User.java
+++ User.java
@@ -1,13 +1,11 @@
 public class User {
-    public User(int id, String name, int age) {
+    public User(int id, String name) {
         this.id = id;
         this.name = name;
-        this.age = age;
     }
 
     int id =1 ;
     String name;
-    int age;
 
     private DB db;
 
@@ -15,8 +13,7 @@ public class User {
 
 
     public void addUser(String user) {
-        db.getUsers().add(user);
-        System.out.println("user added");
+        db.getUsers().add(user + " --- id = "+ id++);
     }
 
     public void listUsers() {
