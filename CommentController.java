8ad0ed8408f94d90561281b14b32fcd297e057e6
Acer-Samsung <bahtiyorov1907@gmail.com>
2025-01-22 16:36:50 +0500

    this should work

--- CommentController.java
+++ CommentController.java
@@ -4,15 +4,25 @@ class Comment {
     int id = 1;
     int user_id;
     String comment;
+
+    public Comment(int user_id, String comment) {
+        this.user_id = user_id;
+        this.comment = comment;
+    }
+    public Comment(){}
 }
 
 public class CommentController {
-    public void addComment(int id, int user_id, String comment) {
-        id += 1;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter user id: ");
-        sc.nextInt();
-        System.out.println("Enter comment: ");
-        sc.nextLine();
+
+    public CommentController(DB db) {this.db =db;}
+
+    private DB db;
+
+    public void addComment(Comment comment) {
+        if (comment.comment == null || comment.comment.isEmpty()) {
+            throw new IllegalArgumentException("Comment cannot be null or empty");
+        }
+        comment.id = comment.id + 1;
+        db.getComments().add("Comment ID: " + comment.id + ", User ID: " + comment.user_id + ", Content: " + comment.comment); // Different logic
     }
 }
--- DB.java
+++ DB.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DB {
+
+    private String dbConName;
+
     private List<String> users = new ArrayList<>();
     private List<String> comments = new ArrayList<>();
 
